bb1b315577d87d34210722cc3127bbaafb41d185
LE Dylan <dylan.le@ecole-89.com>
2022-11-07 12:01:13 +0100

    value

---
+++ get_value.c
@@ -0,0 +1,14 @@
+/*
+ * E89 Pedagogical & Technical Lab
+ * project:     lol
+ * created on:  2022-11-03 - 16:34 +0100
+ * 1st author:  dylan.le - dylan.le
+ * description: loll
+ */
+
+int get_value(int a, int b, float ratio )
+{
+    int nb;
+    nb = (b - a)* ratio + a;
+    return nb;
+}
