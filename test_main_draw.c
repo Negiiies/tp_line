0435bdf6b93aa44b6625a6053ecf97ca9b4f0b94
LE Dylan <dylan.le@ecole-89.com>
2022-11-15 14:42:46 +0100

    update

--- test_main_draw.c
+++ test_main_draw.c
@@ -14,25 +14,40 @@ void stu_draw_line(t_bunny_pixelarray *px,
 int main(void)
 {
     t_bunny_window *win;
-      t_bunny_pixelarray *px;
-      t_bunny_position origin;
-      t_bunny_position pos_a;
-      t_bunny_position pos_b;
+    t_bunny_pixelarray *px;
+    t_bunny_position origin;
+    t_bunny_position pos_a;
+    t_bunny_position pos_b;
+    int l;
+    int h;
 
-     win = bunny_start(800, 600, false, "fl: Open a window tutorial");
-     px = bunny_new_pixelarray(800, 600);
-     origin.x = 0;
-     origin.y = 0;
-     pos_a.x = 800;
-     pos_a.y = 0;
-     pos_b.x = 0;
-     pos_b.y = 600;
-     clear_pixelarray(px, WHITE);
-     // [votre code ou vous modifiez le pixel buffer]
-     stu_draw_line(px, &pos_a, &pos_b, RED);
-     bunny_blit(&win->buffer, &px->clipable, &origin);
-     bunny_display(win);
-     usleep(1e7);
-     bunny_stop(win);
-     return (EXIT_SUCCESS);
+    l = 800;
+    h = 600;
+    win = bunny_start(l, h, false, "fl: Open a window tutorial");
+    px = bunny_new_pixelarray(l, h);
+    origin.x = 0;
+    origin.y = 0;
+    pos_a.x = 0;
+    pos_a.y = 0;
+    pos_b.x = l;
+    pos_b.y = h;
+    clear_pixelarray(px, YELLOW);
+
+     while(pos_a.x < l) {
+    stu_draw_line(px, &pos_a, &pos_b, RED);
+    bunny_blit(&win->buffer, &px->clipable, &origin);
+    bunny_display(win);
+            pos_a.x += 1;
+            pos_b.x -= 1;
+        }
+     while(pos_a.y < h) {
+            stu_draw_line(px, &pos_b, &pos_a, RED);
+            bunny_blit(&win->buffer, &px->clipable, &origin);
+            bunny_display(win);
+            pos_a.y += 1;
+            pos_b.y -= 1;
+        }
+        usleep(1e6);
+        bunny_stop(win);
+        return (EXIT_SUCCESS);
 }
